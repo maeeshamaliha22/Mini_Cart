3624c2b54d06b5b8bc56304ccfe0a9072b8724f2
Maeesha Maliha <maeesha.maliha22@gmail.com>
2021-12-14 18:19:50 +0600

    Adding dummy values in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-
+  <div>
+    <ul>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+  data(){
+    return{
+      storedproducts:[
+        {
+          id: 'apple',
+          title: 'IPhone 13 Pro Max',
+          description: 'The iPhone 13 Pro Max display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle.',
+          image: 'https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg',
+          price: '$1099.00',
+          link: 'https://www.apple.com/iphone-13-pro/specs/',
+        },
+
+        {
+          id: 'google',
+          title: 'Google Pixel 6 Pro',
+          description: 'Google Pixel 6 Pro Android smartphone. Announced Oct 2021. Features 6.71″ display, Google Tensor chipset, 5003 mAh battery, 512 GB storage, 12 GB RAM',
+          image: 'https://fdn2.gsmarena.com/vv/bigpic/google-pixel-6-pro.jpg',
+          price: '$898.98',
+          link: 'https://store.google.com/us/product/pixel_6_pro?hl=en-US'
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
