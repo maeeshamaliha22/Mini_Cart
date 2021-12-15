59d6b327f25a4cceb5e87e10411a075284c0b420
Maeesha Maliha <maeesha.maliha22@gmail.com>
2021-12-15 17:23:30 +0600

    Adding component named CartItem.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
     <ul>
-      <li></li>
+      <cart-item 
+      v-for="storedproduct in storedproducts" 
+      :key="storedproduct.id"
+      :title="storedproduct.title"
+      :description="storedproduct.description"
+      :image="storedproduct.image"
+      :price="storedproduct.price"
+      :link="storedproduct.link"></cart-item>
     </ul>
   </div>
 </template>
 
 <script>
 
+import CartItem from './components/CartItem.vue';
+
 export default {
+  components:{
+    'cart-item': CartItem
+  },
+
   data(){
     return{
       storedproducts:[
@@ -16,7 +29,7 @@ export default {
           id: 'apple',
           title: 'IPhone 13 Pro Max',
           description: 'The iPhone 13 Pro Max display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle.',
-          image: 'https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg',
+          image: require('./assets/apple-iphone-13-pro-max.jpg'),
           price: '$1099.00',
           link: 'https://www.apple.com/iphone-13-pro/specs/',
         },
@@ -25,7 +38,7 @@ export default {
           id: 'google',
           title: 'Google Pixel 6 Pro',
           description: 'Google Pixel 6 Pro Android smartphone. Announced Oct 2021. Features 6.71″ display, Google Tensor chipset, 5003 mAh battery, 512 GB storage, 12 GB RAM',
-          image: 'https://fdn2.gsmarena.com/vv/bigpic/google-pixel-6-pro.jpg',
+          image: require('./assets/google-pixel-6-pro.jpg'),
           price: '$898.98',
           link: 'https://store.google.com/us/product/pixel_6_pro?hl=en-US'
         }
---
+++ src/components/CartItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ title }}</h3>
+                <button>Remove Item</button>
+            </header>
+        </div>
+        <p>{{ description }}</p>
+        <img :src="image">
+        <p>{{ price }}</p>
+        <nav>
+            <a :href="link">View Details</a>
+        </nav>
+    </li>
+</template>
+
+<script>
+export default{
+    // props: ['title', 'description', 'image', 'price', 'link']
+    props:{
+        title:{
+            type:String,
+            required: true
+        },
+
+        description:{
+            type: String,
+            required: true
+        },
+
+        image:{
+            type: String, 
+            required: true
+        },
+
+        price:{
+            type: String,
+            required: true
+        },
+
+        link:{
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
